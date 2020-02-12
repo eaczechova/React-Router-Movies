0eba5f7914bb62f8dd6657913167ad7fbc93a56a
Ewa Czech <ewa.czech@hotmail.com>
2020-02-12 13:41:49 +0100

    add mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = movie => {
+		setSavedList([...savedList, movie]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<Route path="/" exact component={MovieList} />
+			<Route path="/movies/:id" exact component={Movie} />
+		</div>
+	);
 };
 
 export default App;
